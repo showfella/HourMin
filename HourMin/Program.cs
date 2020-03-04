aa5fd097ee6845d18ba3e0f55872a5701ae8136a
showfella <59898522+showfella@users.noreply.github.com>
2020-03-04 11:13:38 -0800

    Add project files.

---
+++ HourMin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Anglediff
+{
+    class HourDiff
+    {
+        static int Anglediff(double h, double m)
+        {
+
+            if (h < 0 || m < 0 ||
+           h > 12 || m > 60)
+                Console.Write("Wrong input");
+
+            if (h == 12)
+                h = 0;
+
+            if (m == 60)
+                m = 0;
+
+            int hour_angle = (int)(0.5 * (h * 60 + m));
+            int minute_angle = (int)(6 * m);
+            
+            int angle = Math.Abs(hour_angle - minute_angle);
+            
+            angle = Math.Min(360 - angle, angle);
+
+            return angle;
+        }
+             public static void Main()
+             {
+            Console.WriteLine(Anglediff(9, 60));
+            Console.Write(Anglediff(3, 30));
+        }
+    }
+
+}
+
+
